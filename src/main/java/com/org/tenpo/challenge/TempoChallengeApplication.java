c2bed940ffd1a16ca6cbf9d1384feab015be6580
Fran Castillo <castillofranciscodaniel@gmail.com>
2023-08-08 15:13:56 -0300

    my first commit

---
+++ src/main/java/com/org/tenpo/challenge/TempoChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.org.tenpo.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempoChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempoChallengeApplication.class, args);
+	}
+
+}
