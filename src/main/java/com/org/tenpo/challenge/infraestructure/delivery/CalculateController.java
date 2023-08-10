f55597167397c446ddc73de5cf6a539ca68e9cdf
Fran Castillo <castillofranciscodaniel@gmail.com>
2023-08-09 21:44:03 -0300

    add rate limit

--- src/main/java/com/org/tenpo/challenge/infraestructure/delivery/CalculateController.java
+++ src/main/java/com/org/tenpo/challenge/infraestructure/delivery/CalculateController.java
@@ -5,9 +5,14 @@ import com.org.tenpo.challenge.core.model.SimplePage;
 import com.org.tenpo.challenge.core.usecase.CalculateCU;
 import com.org.tenpo.challenge.core.usecase.FindPaginatedRequestLogCU;
 import com.org.tenpo.challenge.infraestructure.delivery.dto.CalculateRequest;
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Refill;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 @Controller
 @RestController("calculate")
@@ -17,9 +22,16 @@ public class CalculateController {
 
     private final FindPaginatedRequestLogCU findPaginatedRequestLogCU;
 
+    private final Bucket bucket;
+
     public CalculateController(CalculateCU calculateCU, FindPaginatedRequestLogCU findPaginatedRequestLogCU) {
         this.calculateCU = calculateCU;
         this.findPaginatedRequestLogCU = findPaginatedRequestLogCU;
+
+        Bandwidth limit = Bandwidth.classic(3, Refill.greedy(3, Duration.ofMinutes(1)));
+        this.bucket = Bucket.builder()
+                .addLimit(limit)
+                .build();
     }
 
     @PostMapping
