91885cfa4dae17accc5adb440e7b92d38d52fe93
Fran Castillo <castillofranciscodaniel@gmail.com>
2023-08-10 16:23:51 -0300

    add reactive redis to cache data

---
+++ src/main/java/com/org/tenpo/challenge/core/model/ExternalValue.java
@@ -0,0 +1,31 @@
+package com.org.tenpo.challenge.core.model;
+
+import java.util.Date;
+
+public class ExternalValue {
+
+    private Double percentage;
+
+    private Date createdAt;
+
+    public ExternalValue(Double percentage) {
+        this.percentage = percentage;
+        this.createdAt = new Date();
+    }
+
+    public Double getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(Double percentage) {
+        this.percentage = percentage;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+}
--- src/main/java/com/org/tenpo/challenge/infraestructure/config/SpringConfiguration.java
+++ src/main/java/com/org/tenpo/challenge/infraestructure/config/SpringConfiguration.java
@@ -1,5 +1,6 @@
 package com.org.tenpo.challenge.infraestructure.config;
 
+import com.org.tenpo.challenge.core.model.ExternalValue;
 import com.org.tenpo.challenge.core.port.ExternalInformationRepository;
 import com.org.tenpo.challenge.core.port.RequestLogRepository;
 import com.org.tenpo.challenge.core.usecase.CalculateCU;
@@ -7,6 +8,12 @@ import com.org.tenpo.challenge.core.usecase.FindPaginatedRequestLogCU;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisOperations;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -31,6 +38,18 @@ public class SpringConfiguration {
     @Bean
     public CalculateCU newCalculateCU(RequestLogRepository requestLogRepository, ExternalInformationRepository externalInformationRepository) {
         return new CalculateCU(requestLogRepository, externalInformationRepository);
+    }
+
+    @Bean
+    public ReactiveRedisOperations<String, ExternalValue> redisOperations(ReactiveRedisConnectionFactory factory) {
+        Jackson2JsonRedisSerializer<ExternalValue> serializer = new Jackson2JsonRedisSerializer<>(ExternalValue.class);
+
+        RedisSerializationContext.RedisSerializationContextBuilder<String, ExternalValue> builder =
+                RedisSerializationContext.newSerializationContext(new StringRedisSerializer());
+
+        RedisSerializationContext<String, ExternalValue> context = builder.value(serializer).build();
+
+        return new ReactiveRedisTemplate<>(factory, context);
     }
 
 }
