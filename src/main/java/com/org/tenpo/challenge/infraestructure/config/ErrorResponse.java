5229ae8fe20906f08a67e3266e38e925c9da0c0e
Fran Castillo <castillofranciscodaniel@gmail.com>
2023-08-12 00:49:48 -0300

    first version of error global handler

---
+++ src/main/java/com/org/tenpo/challenge/infraestructure/config/ErrorResponse.java
@@ -0,0 +1,39 @@
+package com.org.tenpo.challenge.infraestructure.config;
+
+public class ErrorResponse {
+
+    private int errorCode;
+    private String message;
+
+    public ErrorResponse() {
+    }
+
+    public ErrorResponse(int errorCode, String message) {
+        this.errorCode = errorCode;
+        this.message = message;
+    }
+
+    public int getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(int errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "ErrorResponse{" +
+                "errorCode=" + errorCode +
+                ", message='" + message + '\'' +
+                '}';
+    }
+}
